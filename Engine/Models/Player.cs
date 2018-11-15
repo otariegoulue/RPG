a53866a1e355ac86b8d9d90caaf408ec8c8f0be2
otariegoulue <otariegoulue@gmail.com>
2018-11-15 10:17:24 +0100

    Modif

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Player
+    {
+
+        public string name { get; set; }
+        public string characterClass { get; set; }
+        public int HitPoints { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.name = "Seb";
+            CurrentPlayer.Gold = 100000;
+        }
+    }  
+}
